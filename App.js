cf0f88870d8820f21a25f025f191a1f1565874c1
abhi-s-a <65067486+abhi-s-a@users.noreply.github.com>
2021-08-22 11:02:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Resturant from './components/basics/Resturant';
+import UseState from './components/Hooks/useState';
+import UseEffect from './components/Hooks/useEffect';
+import UseReducer from './components/Hooks/useReducer';
+import Todo from './components/Notes/todo';
+const App = () => {
+    return <Todo />;
+}
+
+export default App
